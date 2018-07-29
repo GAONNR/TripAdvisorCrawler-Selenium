ab926408f0d1f105aa132a7b281f0dce7f0cf6a7
G40N <gaon0403@gmail.com>
2018-07-29 15:29:53 +0900

    Add function to analyze nouns' frequency

--- tagger.py
+++ tagger.py
@@ -2,19 +2,31 @@ from konlpy.tag import Twitter
 import glob
 import sys
 import json
+import csv
 
 
 def tag_all_reviews(norm, stem):
     twitter = Twitter()
     recommend_categories = set()
+    nouns = dict()
+
     for filename in glob.glob('reviews/*.json'):
         with open(filename, 'r') as raw_file:
             print('parsing %s...' % filename)
             raw_data = json.load(raw_file)
 
             for review in raw_data:
-                review['tagged'] = twitter.pos(
+                raw_tags = twitter.pos(
                     review['text'], norm=norm, stem=stem)
+                review['tagged'] = list()
+                for tag in raw_tags:
+                    if tag[1] in ['Noun', 'Adjective', 'Verb']:
+                        review['tagged'].append(tag)
+                    if tag[1] == 'Noun':
+                        if tag[0] in nouns:
+                            nouns[tag[0]] += 1
+                        else:
+                            nouns[tag[0]] = 0
                 recommend_categories.update(list(review['recommend'].keys()))
 
             new_filename = 'tagged_reviews/%s' % filename.split('/')[1]
@@ -22,6 +34,14 @@ def tag_all_reviews(norm, stem):
                 json.dump(raw_data, tagged_file, ensure_ascii=False,
                           sort_keys=True, indent=2, separators=(',', ': '))
 
+    c = 0
+    with open('nouns.csv', 'w') as nouns_file:
+        nf = csv.writer(nouns_file)
+        for key in nouns.keys():
+            if nouns[key] >= 100:
+                c += 1
+                nf.writerow([key, nouns[key]])
+    print(c)
     return recommend_categories
 
 
